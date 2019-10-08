54c8ba3ce418eb432dbabf80e1de2e960f67da9d
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-10-08 13:49:59 -0700

    basic CRUD functionality working with MongoDB atlas as a Database

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Harry-Potter-Store
+
+
+credits:
+https://harrypotter.fandom.com/wiki/List_of_spells
--- app.py
+++ app.py
@@ -1,19 +1,38 @@
+from flask import Flask
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://abhi:contrapro472@contractor-l3tgn.mongodb.net/admin?retryWrites=true&w=majority")
+app = Flask(__name__)
 
+cluster = MongoClient("mongodb+srv://abhi:contrapro472@contractor-l3tgn.mongodb.net/admin?retryWrites=true&w=majority")
 db = cluster["testdb"]
 collection = db["testcollection"]
 
 post = {
+	"_id": 2,
 	"name": "Abhi",
 	"fav drink": "Chai",
-	"facebook": "https://www.facebook.com/jesse.phoenix.3154"
 }
 
-update = {
-	"name": "Abhishek"
-}
-collection.insert_one(post)
-collection.update_one({"_id": 0}, {"$set": {"name": "Abhishek"}})
+
+@app.route('/create')
+def create():
+	collection.insert_one(post)
+	return '<h1>One post Inserted</h1>'
+	
+
+@app.route('/update')
+def update():
+	collection.update_one({"_id": 1}, {"$set": {"name": "Abhishek Kulkarni"}})
+	return '<h1>One post updated</h1>'
+
+
+@app.route('/delete')
+def delete():
+	collection.delete_one({"_id": 2})
+	return 'Delete Successful'
+
+
+@app.route('/home')
+def index():
+	return f'<h1>HOME</h1>'
 
