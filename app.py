3805b5af354d59c8cab9e9a90365a9711eb78b2e
Abhishek5101 <miabhishekkulkarni@gmail.com>
2019-10-10 22:49:31 -0700

    Final Commit

--- app.py
+++ app.py
@@ -17,49 +17,41 @@ cluster = MongoClient("mongodb+srv://abhi:contrapro472@contractor-l3tgn.mongodb.
 db = cluster["store"]
 magic_collection = db["magic"]
 
-
-# cart = db["shopping cart"]
-
-# post = [
-# 	{"_id": 1, "name": "love".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Harry-Potter-and-Ginny-Weasley-in-Love.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 2, "name": "CONJURING".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Snake-from-Harry-Potter-and-the-Chamber-of-Secrets.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 3, "name": "SPELL INVENTION".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Snake-from-Harry-Potter-and-the-Chamber-of-Secrets.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 4, "name": "RUDIMENTARY MAGIC".capitalize(), "image": "https://static3.srcdn.com/wordpress/wp-content/uploads/2016/11/Troll-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 5, "name": "NAMING SEER".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Harry-Potter-Crystal-Ball.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 6, "name": "MAGICAL RESISTANCE".capitalize(), "image": "https://static3.srcdn.com/wordpress/wp-content/uploads/2016/11/Harry-Potter-Magical-Resistance.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 7, "name": "VEELA CHARM".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Fleur-Delacour-in-Harry-Potter-Veela-Charm.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 8, "name": "APPARITION AND DISAPPARITION".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Apparition-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 9, "name": "UNBREAKABLE VOW".capitalize(), "image": "https://static1.srcdn.com/wordpress/wp-content/uploads/2016/11/An-Unbreakable-Vow-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 10, "name": "FLIGHT".capitalize(), "image": "https://static2.srcdn.com/wordpress/wp-content/uploads/2016/11/Snape-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 11, "name": "SEERS".capitalize(), "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2016/11/Trelawney-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 12, "name": "ANIMAGI".capitalize(), "image": "https://static2.srcdn.com/wordpress/wp-content/uploads/2016/11/Sirius-Black-Metamorphmagi-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 13, "name": "METAMORPHMAGI".capitalize(), "image": "https://static3.srcdn.com/wordpress/wp-content/uploads/2016/11/Metamorphmagus-in-Harry-Potter.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 14, "name": "OCCLUMENCY".capitalize(), "image": "https://static3.srcdn.com/wordpress/wp-content/uploads/2016/11/Harry-Potter-Occlumency.jpg?q=50&fit=crop&w=738&h=369"},
-# 	{"_id": 15, "name": "LEGITIMACY".capitalize(), "image": "https://static3.srcdn.com/wordpress/wp-content/uploads/2016/11/Harry-Potter-Legilimency.jpg?q=50&fit=crop&w=738&h=369"}
-#
-# ]
-
+"""
+The Index Route will show all the items
+"""
 @app.route('/')
 def show_magics():
 	return render_template('magics_index.html', magics=magic_collection.find())
 
-
+"""
+The About Route will go to a diferent HTML document that explains more about the website
+"""
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
 
+"""
+The main read Route will show all the items
+"""
 @app.route('/magics')
 def magic():
 	magics = list(magic_collection.find({}))
 	return render_template('magics_index.html', magics=magic_collection.find())
 
 
+"""
+The new Route create a new item with a form from the provided HTML
+"""
 @app.route('/magics/new')
 def new_magic():
 	return render_template('magics_new.html')
 
 
+"""
+Post method for the same route to use the information passed in from the Form
+"""
 @app.route('/magics', methods=["POST"])
 def magic_create():
 	new_magic = {
@@ -71,6 +63,9 @@ def magic_create():
 	return render_template('magics_index.html', magics=magic_collection.find())
 
 
+"""
+The delete route to delete the item with the specific ID
+"""
 @app.route('/delete/<magic_id>', methods=['POST'])
 def magic_delete(magic_id):
 	"""Delete one guitar."""
@@ -88,7 +83,10 @@ def update_magic(magic_id):
 	else:
 		return redirect(url_for('magic'))
 	
-	
+
+"""
+The update route to update the item with the specific ID
+"""
 @app.route('/update/<magic_id>', methods=['GET', "POST"])
 def magic_update(magic_id):
 	if request.method == 'POST':
